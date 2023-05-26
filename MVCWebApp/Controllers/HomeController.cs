e2f634c170a4a339f73d4c06faef9209d4d15798
Regie <regie@callhandling.co.uk>
2023-05-26 21:54:50 +0800

    Add project files.

---
+++ MVCWebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using MVCWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var authors = new List<Authors>() { 
+             new Authors{ Name = "Author 1", Address="missing st."},
+             new Authors{ Name = "Author 2", Address="missing st."},
+             new Authors{ Name = "Author 3", Address="missing st."},
+            };
+            return View(authors);
+        }
+    }
+}
---
+++ MVCWebApp/Models/Authors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebApp.Models
+{
+    public class Authors
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
